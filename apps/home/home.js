b7419bc896746acadfc569893e6ddfa3f260bb33
Seamus O Connor <seamuso@kevgroup.com>
2016-11-07 19:02:21 -0500

    Created ui's and added functionality to home page

---
+++ apps/home/home.js
@@ -0,0 +1,24 @@
+/**
+ * Created by seamuso on 11/7/2016.
+ */
+
+
+$(document).ready(function(){
+    $("input[type='button']").click(function() {
+
+    });
+});
+
+
+function changeUrl(url) {
+        console.log("Seamy")
+    switch(url) {
+
+        case 'btnSearchJobs':window.location.replace("http://stackoverflow.com"); break;
+        case 'btnSavedJobs': window.location.replace("http://stackoverflow.com"); break;
+        case 'btnJobAlerts':window.location.replace("http://stackoverflow.com"); break;
+        case 'btnEditProfile': window.location.replace("http://stackoverflow.com"); break;
+        case 'btnContactUs': window.location.replace("http://stackoverflow.com"); break;
+    }
+
+}
